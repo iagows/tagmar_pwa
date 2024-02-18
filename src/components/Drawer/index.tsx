cf3b25b9b1a1e844d16873a2bad1dfe1d9178243
Iago <el.iagows@gmail.com>
2024-02-18 15:30:47 -0300

    feat: menu

--- src/components/Drawer/index.tsx
+++ src/components/Drawer/index.tsx
@@ -1,3 +1,4 @@
+import InboxIcon from "@mui/icons-material/MoveToInbox";
 import {
 	Divider,
 	List,
@@ -5,44 +6,77 @@ import {
 	ListItemButton,
 	ListItemIcon,
 	ListItemText,
+	SvgIconTypeMap,
 	Toolbar,
 } from "@mui/material";
+import { OverridableComponent } from "@mui/material/OverridableComponent";
 
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
+type Item = {
+	text: string;
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	icon: OverridableComponent<SvgIconTypeMap<any, "svg">> & { muiName: string };
+};
+
+const gameMenu: Item[] = [
+	{
+		text: "Fichas",
+		icon: InboxIcon,
+	},
+	{
+		text: "Ficha atual",
+		icon: InboxIcon,
+	},
+	{
+		text: "Magias",
+		icon: InboxIcon,
+	},
+	{
+		text: "Habilidades",
+		icon: InboxIcon,
+	},
+	{
+		text: "Mapa",
+		icon: InboxIcon,
+	},
+];
+
+const appMenu: Item[] = [
+	{
+		text: "Configurações",
+		icon: InboxIcon,
+	},
+	{
+		text: "Sobre",
+		icon: InboxIcon,
+	},
+];
+
+type IList = {
+	list: Item[];
+};
+const MountList = ({ list }: IList) => (
+	<List>
+		{list.map(({ text, icon: Icon }) => (
+			<ListItem key={text}>
+				<ListItemButton>
+					<ListItemIcon>
+						<Icon />
+					</ListItemIcon>
+					<ListItemText primary={text} />
+				</ListItemButton>
+			</ListItem>
+		))}
+	</List>
+);
 
 const Drawer = () => {
 	return (
 		<>
 			<Toolbar />
 			<Divider />
-			<List>
-				{["Fichas", "Ficha Atual", "Magias", "Habilidades", "Mapa"].map(
-					(text, index) => (
-						<ListItem key={text} disablePadding>
-							<ListItemButton>
-								<ListItemIcon>
-									{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-								</ListItemIcon>
-								<ListItemText primary={text} />
-							</ListItemButton>
-						</ListItem>
-					),
-				)}
-			</List>
+			<MountList list={gameMenu} />
 			<Divider />
-			<List>
-				{["Configurações", "Sobre"].map((text, index) => (
-					<ListItem key={text} disablePadding>
-						<ListItemButton>
-							<ListItemText primary={text} />
-							<ListItemIcon>
-								{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-							</ListItemIcon>
-						</ListItemButton>
-					</ListItem>
-				))}
-			</List>
+			<MountList list={appMenu} />
 		</>
 	);
 };
