5ff603f07d028b9568bf4f46b846551ff886bb79
Iago <el.iagows@gmail.com>
2024-03-03 11:30:11 -0300

    feat: lista de magias

---
+++ src/components/ListaDeLetras/index.tsx
@@ -0,0 +1,29 @@
+import { Box, Button } from "@mui/material";
+import { extractFirstChar } from "../../util/functions";
+
+type HasName = {
+	nome: string;
+};
+
+type In = {
+	lista: HasName[];
+};
+
+const getChar = (obj: HasName): string => extractFirstChar(obj.nome);
+const getCharSet = (lista: HasName[]): Set<string> =>
+	new Set(lista.map(getChar));
+const getCharList = (lista: HasName[]): string[] => [...getCharSet(lista)];
+
+const ListaDeLetras = ({ lista }: In) => {
+	const letras = getCharList(lista);
+
+	return (
+		<Box sx={{ gap: 1, display: "flex" }}>
+			{letras.map((l) => (
+				<Button variant="outlined">{l}</Button>
+			))}
+		</Box>
+	);
+};
+
+export default ListaDeLetras;
--- src/pages/PageMagias.tsx
+++ src/pages/PageMagias.tsx
@@ -1,35 +1,31 @@
-import { InputAdornment, TextField } from "@mui/material";
-import PageContainer from "../components/PageContainer";
 import SearchOutlined from "@mui/icons-material/SearchOutlined";
+import { Box, InputAdornment, TextField } from "@mui/material";
+import ListaDeLetras from "../components/ListaDeLetras";
+import PageContainer from "../components/PageContainer";
 import { MAGIAS } from "../data/magias";
-import { extractFirstChar } from "../util/functions";
-
-const LETRAS = [...new Set(MAGIAS.map((m) => extractFirstChar(m.nome)))];
 
 const PageMagias = () => {
 	return (
 		<PageContainer>
-			<TextField
-				label="Nome da magia"
-				variant="outlined"
-				InputProps={{
-					endAdornment: (
-						<InputAdornment position="start">
-							<SearchOutlined />
-						</InputAdornment>
-					),
-				}}
-			/>
-			<div>
-				{LETRAS.map((i) => (
-					<p key={i}>{i}</p>
-				))}
-			</div>
-			<div style={{ display: "flex", gap: 10 }}>
-				{MAGIAS.map((m) => (
-					<p key={m.id}>{m.nome}</p>
-				))}
-			</div>
+			<Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+				<TextField
+					label="Nome da magia"
+					variant="outlined"
+					InputProps={{
+						endAdornment: (
+							<InputAdornment position="start">
+								<SearchOutlined />
+							</InputAdornment>
+						),
+					}}
+				/>
+				<ListaDeLetras lista={MAGIAS} />
+				<Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
+					{MAGIAS.map((m) => (
+						<p key={m.id}>{m.nome}</p>
+					))}
+				</Box>
+			</Box>
 		</PageContainer>
 	);
 };
