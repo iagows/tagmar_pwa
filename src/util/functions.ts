837d18797aaa0477adf5744872907dd120affdf3
Iago <el.iagows@gmail.com>
2024-03-03 12:37:58 -0300

    feat: funções para strings

--- src/util/functions.ts
+++ src/util/functions.ts
@@ -8,27 +8,3 @@ export const toArray = <T>(obj: SingleOrArray<T>): T[] => {
 export const changeOpacity = (color: string, alpha: number): string => {
 	return Color.createFrom(color).changeAlpha(alpha).toRgba();
 };
-
-export const getPlural = (n: number): "s" | "" => {
-	return n > 0 ? "s" : "";
-};
-
-function removeDiacritic(letra: string): string {
-	const diacriticsMap: { [key: string]: string } = {
-		á: "a",
-		à: "a",
-		â: "a",
-		é: "e",
-		ê: "e",
-		í: "i",
-		ó: "o",
-		ô: "o",
-		ú: "u",
-		ç: "c",
-	};
-	return diacriticsMap[letra] || letra;
-}
-
-export const extractFirstChar = (word: string): string => {
-	return removeDiacritic(word.trim().charAt(0).toLowerCase());
-};
