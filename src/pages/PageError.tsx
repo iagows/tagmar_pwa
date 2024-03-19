9ab6ecee60e43b2eabc34ad36a42c8cfa6e7da37
Iago <el.iagows@gmail.com>
2024-03-19 17:47:43 -0300

    fix: exibir erro

--- src/pages/PageError.tsx
+++ src/pages/PageError.tsx
@@ -1,10 +1,17 @@
+import { Link, useLocation, useRouteError } from "react-router-dom";
+
 const PageError = () => {
 	// mudar menu
 	// mudar app bar
 	// mudar head > title
+	const { pathname } = useLocation();
+	const error = useRouteError() as undefined | { message: string };
 	return (
 		<div>
-			<p>Erro</p>
+			<p>
+				{pathname} - Erro: {error?.message}
+			</p>
+			<Link to="/">Go back to Home Page</Link>
 		</div>
 	);
 };
