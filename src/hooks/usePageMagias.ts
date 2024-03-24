7d224539d59186af06a8cb7ff8b70f36107fa300
Iago <el.iagows@gmail.com>
2024-03-24 13:36:09 -0300

    refactor: moved logic to hook

---
+++ src/hooks/usePageMagias.ts
@@ -0,0 +1,98 @@
+import { useState } from "react";
+import { MAGIAS } from "../data/magiasCompiladas";
+import { Relations } from "../data/relations";
+import { AbstractDTO as IdDTO } from "../models/Abstract/IdDTO";
+import { AbstractDTO as NameDTO } from "../models/Abstract/NameDTO";
+import { ProfissaoEnum } from "../models/ProfissaoDTO";
+import { Magia, MagiaEnum } from "../models/magia/MagiaDTO";
+import { VoidCallback } from "../util/commonTypes";
+import { swapSet } from "../util/functions";
+import { StringUtil } from "../util/stringHelp";
+
+const filterByProfissao = (profs: ProfissaoEnum[], item: MagiaEnum): boolean =>
+	Relations.filterMagiaPor(profs, item);
+
+type Out = {
+	input: {
+		text: string;
+		setText: (s: string) => void;
+		clear: VoidCallback;
+		hasText: boolean;
+	};
+	list: {
+		data: Magia[];
+	};
+	char: {
+		char: string;
+		onCharClick: (c: string) => void;
+	};
+	filter: {
+		clear: VoidCallback;
+		swap: (prof: ProfissaoEnum) => void;
+		exists: (prof: ProfissaoEnum) => boolean;
+	};
+};
+const usePageMagias = (): Out => {
+	const [text, setText] = useState<string>("");
+	const [selectedCharButton, setSelectedCharButton] = useState<string>("");
+	const [selectedProf, setSelectedProf] = useState<Set<ProfissaoEnum>>(
+		new Set(),
+	);
+
+	const profFilter = (i: IdDTO.IdType) =>
+		filterByProfissao([...selectedProf], i.id as MagiaEnum);
+	const charFilter = (i: NameDTO.NameType) =>
+		StringUtil.filterByFirstChar(selectedCharButton, i.nome);
+	const proxFilter = (i: NameDTO.NameType) =>
+		StringUtil.filterByProximity(text, i.nome);
+
+	const filtered = MAGIAS.filter(profFilter)
+		.filter(charFilter)
+		.filter(proxFilter);
+
+	function clearText() {
+		setText("");
+		setSelectedCharButton("");
+	}
+
+	function clearFilters() {
+		setSelectedProf(new Set());
+	}
+
+	function onCharClick(char: string): void {
+		setSelectedCharButton((last) => (char === last ? "" : char));
+	}
+
+	function swap(prof: ProfissaoEnum): void {
+		setSelectedProf((last) => swapSet(last, prof));
+	}
+
+	function exists(prof: ProfissaoEnum): boolean {
+		return selectedProf.has(prof);
+	}
+
+	const hasText: boolean = text.length > 0 || selectedCharButton.length > 0;
+
+	return {
+		input: {
+			clear: clearText,
+			hasText,
+			setText,
+			text,
+		},
+		list: {
+			data: filtered,
+		},
+		char: {
+			char: selectedCharButton,
+			onCharClick,
+		},
+		filter: {
+			clear: clearFilters,
+			swap,
+			exists,
+		},
+	};
+};
+
+export default usePageMagias;
--- src/pages/PageMagias.tsx
+++ src/pages/PageMagias.tsx
@@ -1,88 +1,37 @@
 import { Avatar, Badge, Box, MenuItem } from "@mui/material";
-import { useState } from "react";
 import getAsset from "../assets/app";
 import ListaDeLetras from "../components/ListaDeLetras";
 import ListaDeNomes from "../components/ListaDeNomes";
 import PageContainer from "../components/PageContainer";
 import TextFieldWithFilter from "../components/TagmarUI/TagLista/TextFieldWithFilter";
 import UnderConstruction from "../components/UnderConstruction";
-import { MAGIAS } from "../data/magiasCompiladas";
 import { Relations } from "../data/relations";
-import { AbstractDTO as IdDTO } from "../models/Abstract/IdDTO";
-import { AbstractDTO as NameDTO } from "../models/Abstract/NameDTO";
-import { ProfissaoEnum } from "../models/ProfissaoDTO";
-import { MagiaEnum } from "../models/magia/MagiaDTO";
+import usePageMagias from "../hooks/usePageMagias";
 import { RoutePath } from "../routing/RouteNames";
-import { StringUtil } from "../util/stringHelp";
-
-const createSet = <T,>(s: Set<T>, o: T): Set<T> => {
-	if (s.has(o)) {
-		s.delete(o);
-	} else {
-		s.add(o);
-	}
-	return new Set([...s]);
-};
-
-const filterByProfissao = (profs: ProfissaoEnum[], item: MagiaEnum): boolean =>
-	Relations.filterMagiaPor(profs, item);
 
 const profissoes = Relations.getProfissoesMagicas();
 
 const PageMagias = () => {
-	const [text, setText] = useState<string>("");
-	const [selectedCharButton, setSelectedCharButton] = useState<string>("");
-	const [selectedProf, setSelectedProf] = useState<Set<ProfissaoEnum>>(
-		new Set(),
-	);
-
-	const profFilter = (i: IdDTO.IdType) =>
-		filterByProfissao([...selectedProf], i.id as MagiaEnum);
-	const charFilter = (i: NameDTO.NameType) =>
-		StringUtil.filterByFirstChar(selectedCharButton, i.nome);
-	const proxFilter = (i: NameDTO.NameType) =>
-		StringUtil.filterByProximity(text, i.nome);
-
-	const filtered = MAGIAS.filter(profFilter)
-		.filter(charFilter)
-		.filter(proxFilter);
-
-	function clearText() {
-		setText("");
-		setSelectedCharButton("");
-	}
-
-	function clearFilters() {
-		setSelectedProf(new Set());
-	}
-
-	function onCharClick(char: string): void {
-		setSelectedCharButton((last) => (char === last ? "" : char));
-	}
-
-	const hasText: boolean = text.length > 0 || selectedCharButton.length > 0;
+	const { input, list, char, filter } = usePageMagias();
 
 	return (
 		<PageContainer>
-			<UnderConstruction descricao="Revisar magias e otimizar renderização da lista" />
+			<UnderConstruction descricao="Revisar magias" />
 			<Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
 				<TextFieldWithFilter
-					text={text}
-					onChange={setText}
+					text={input.text}
 					label="Nome da magia"
-					onClearText={clearText}
-					showClearButton={hasText}
-					onClearFilters={clearFilters}
+					onChange={input.setText}
+					onClearText={input.clear}
+					onClearFilters={filter.clear}
+					showClearButton={input.hasText}
 				>
 					{profissoes.map((p) => {
 						const Icon = getAsset(p);
-						const hideBadge = !selectedProf.has(p);
+						const hideBadge = !filter.exists(p);
 
-						const swap = () => {
-							setSelectedProf((last) => createSet(last, p));
-						};
 						return (
-							<MenuItem key={p} onClick={swap}>
+							<MenuItem key={p} onClick={() => filter.swap(p)}>
 								<Badge
 									variant="dot"
 									color="primary"
@@ -100,11 +49,11 @@ const PageMagias = () => {
 					})}
 				</TextFieldWithFilter>
 				<ListaDeLetras
-					lista={filtered}
-					onClick={onCharClick}
-					selected={selectedCharButton}
+					lista={list.data}
+					onClick={char.onCharClick}
+					selected={char.char}
 				/>
-				<ListaDeNomes lista={filtered} to={RoutePath.MAGIA} />
+				<ListaDeNomes lista={list.data} to={RoutePath.MAGIA} />
 			</Box>
 		</PageContainer>
 	);
